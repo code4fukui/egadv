7cb317035f6445519ea86b5cdbac45a435fd996d
Taisuke Fukuno <fukuno@jig.jp>
2021-05-11 09:07:10 +0900

    fmt

--- demo2.js
+++ demo2.js
@@ -1,14 +1,14 @@
 alert("おはよう、朝だ");
 for (;;) {
-	const n = prompt("どこへいこう？\n1. 山\n2. 川\n3. 帰る");
-	if (n == 1) {
-		alert("山についた");
-		alert("HPが10、回復した");
-	} else if (n == 2) {
-		alert("川についた");
-		alert("MPが5、回復した");
-	} else {
-		break;
-	}
+  const n = prompt("どこへいこう？\n1. 山\n2. 川\n3. 帰る");
+  if (n == 1) {
+    alert("山についた");
+    alert("HPが10、回復した");
+  } else if (n == 2) {
+    alert("川についた");
+    alert("MPが5、回復した");
+  } else {
+    break;
+  }
 }
 alert("もう、夜だ、おやすみ");
