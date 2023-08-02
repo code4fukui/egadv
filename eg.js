0c8187049ae6ef1bb627d47ccc4aa38de62bcd2f
Taisuke Fukuno <fukuno@jig.jp>
2023-08-02 21:42:39 +0900

    add bg8 on eg.js

--- eg.js
+++ eg.js
@@ -1,7 +1,8 @@
-import { show, bg, map, rnd } from "./egadv.js";
+import { show, bg, bg8, map, rnd } from "./egadv.js";
 
 window.show = show;
 window.bg = bg;
+window.bg8 = bg8;
 window.map = map;
 window.rnd = rnd;
 
